62eff1f79ee5ada1da78e7f7e6fade6e91985245
nicholas <bul-nick-al@ya.ru>
2019-03-20 18:03:39 +0300

    add prettier

--- src/app/components/user-sidebar/user-sidebar.component.ts
+++ src/app/components/user-sidebar/user-sidebar.component.ts
@@ -1,40 +1,37 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {NbMenuItem, NbMenuService} from '@nebular/theme';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { NbMenuItem, NbMenuService } from "@nebular/theme";
 
 @Component({
-  selector: 'app-user-sidebar',
-  templateUrl: './user-sidebar.component.html',
-  styleUrls: ['./user-sidebar.component.css']
+    selector: "app-user-sidebar",
+    templateUrl: "./user-sidebar.component.html",
+    styleUrls: ["./user-sidebar.component.css"],
 })
 export class UserSidebarComponent implements OnInit {
+    items: NbMenuItem[];
+    @Output() chosen = new EventEmitter<string>();
 
-  items: NbMenuItem[];
-  @Output() chosen = new EventEmitter<string>();
+    constructor(menu: NbMenuService) {
+        menu.onItemClick().subscribe(({ item }) => {
+            this.chosen.emit(item.title);
+        });
+        this.items = [
+            {
+                icon: "fas fa-file-invoice",
+                title: "Documents",
+                link: "",
+            },
+            {
+                icon: "fas far fa-edit",
+                title: "Tests",
+                link: "",
+            },
+            {
+                icon: "fas fa-user",
+                title: "Interviews",
+                link: "",
+            },
+        ];
+    }
 
-  constructor(menu: NbMenuService) {
-    menu.onItemClick().subscribe(({item}) => {
-      this.chosen.emit(item.title);
-    });
-    this.items = [
-      {
-        icon: 'fas fa-file-invoice',
-        title: 'Documents',
-        link: '',
-      },
-      {
-        icon: 'fas far fa-edit',
-        title: 'Tests',
-        link: '',
-      },
-      {
-        icon: 'fas fa-user',
-        title: 'Interviews',
-        link: '',
-      }
-    ];
-  }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
--- src/app/pages/candidate-page/candidate-page.component.ts
+++ src/app/pages/candidate-page/candidate-page.component.ts
@@ -1,29 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-candidate-page',
-  templateUrl: './candidate-page.component.html',
-  styleUrls: ['./candidate-page.component.css']
+    selector: "app-candidate-page",
+    templateUrl: "./candidate-page.component.html",
+    styleUrls: ["./candidate-page.component.css"],
 })
 export class CandidatePageComponent implements OnInit {
+    items = [
+        { title: "Notification 1" },
+        { title: "A looooong text of a notification" },
+        { title: "Notification 2" },
+    ];
 
-  items = [
-    { title: 'Notification 1' },
-    { title: 'A looooong text of a notification' },
-    { title: 'Notification 2' },
-  ];
+    currentSubPage = "Documents";
 
-  currentSubPage = 'Documents';
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-
-
-  setSubPage(name: string) {
-    this.currentSubPage = name;
-  }
-
+    setSubPage(name: string) {
+        this.currentSubPage = name;
+    }
 }
