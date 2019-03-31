cbcd9f0a446c92f20cd0f5d562ac4a148818149c
Georgiy Dzesov <g.dzesov@gmail.com>
2019-03-31 12:40:23 +0300

    restyling

--- src/app/+state/tests/tests.selectors.ts
+++ src/app/+state/tests/tests.selectors.ts
@@ -1,6 +1,8 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromTests from './tests.reducer';
+import { TestsState } from './tests.reducer';
 
 export const getTestsState = createFeatureSelector<fromTests.TestsState>('tests');
 
 export const getTestsPool = createSelector(getTestsState, fromTests.selectAll);
+export const getIsTestsLoading = createSelector(getTestsState, (state: TestsState) => state.isLoading);
