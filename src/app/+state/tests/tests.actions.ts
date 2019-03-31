8c4e23cf6338d99a42f2f922d5114bbecb3eb04a
nicholas <bul-nick-al@ya.ru>
2019-03-31 13:37:54 +0300

    add interfaces

--- src/app/+state/tests/tests.actions.ts
+++ src/app/+state/tests/tests.actions.ts
@@ -1,33 +1,30 @@
-import { Action } from '@ngrx/store';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Action } from "@ngrx/store";
+import { HttpErrorResponse } from "@angular/common/http";
 
 export enum TestsActionTypes {
-  LOAD_TESTS_POOL = '[Tests] Load Tests Pool',
-  LOAD_TESTS_POOL_SUCCESS = '[Tests] Load Tests Pool Success',
-  LOAD_TESTS_POOL_FAIL = '[Tests] Load Test Fail',
+    LOAD_TESTS_POOL = "[Tests] Load Tests Pool",
+    LOAD_TESTS_POOL_SUCCESS = "[Tests] Load Tests Pool Success",
+    LOAD_TESTS_POOL_FAIL = "[Tests] Load Test Fail",
 }
 
 export class TestsAction implements Action {
-  readonly type: TestsActionTypes;
+    readonly type: TestsActionTypes;
 }
 
 export class LoadTestsPool implements Action {
-  readonly type: TestsActionTypes;
+    readonly type: TestsActionTypes;
 
-  constructor(public payload = null) {
-  }
+    constructor(public payload = null) {}
 }
 
 export class LoadTestsPoolSuccess implements Action {
-  readonly type: TestsActionTypes;
+    readonly type: TestsActionTypes;
 
-  constructor(public payload: { tests: TestInfoInterface[] }) {
-  }
+    constructor(public payload: { tests: TestInfoInterface[] }) {}
 }
 
 export class LoadTestsPoolFail implements Action {
-  readonly type: TestsActionTypes;
+    readonly type: TestsActionTypes;
 
-  constructor(public payload: { error: HttpErrorResponse }) {
-  }
+    constructor(public payload: { error: HttpErrorResponse }) {}
 }
