a5148bb70469dbed62db4704965f9f3ff1ac10bd
Anastasia Reprintseva <anastasia.vorton@gmail.com>
2019-04-02 18:58:59 +0300

    Add services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,42 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SharedModule } from "./components/shared.module";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SharedModule} from './components/shared.module';
 
 import {
-    NbLayoutModule,
-    NbMenuModule,
-    NbSidebarModule,
-    NbThemeModule,
-} from "@nebular/theme";
-import { LoginPageModule } from "./pages/login-page/login-page.module";
-import { CandidatePageModule } from "./pages/candidate-page/candidate-page.module";
-import { ManagerPageModule } from "./pages/manager-page/manager-page.module";
-import { StoreModule } from "@ngrx/store";
-import { reducers } from "./+state";
+  NbLayoutModule,
+  NbMenuModule,
+  NbSidebarModule,
+  NbThemeModule,
+} from '@nebular/theme';
+import {LoginPageModule} from './pages/login-page/login-page.module';
+import {CandidatePageModule} from './pages/candidate-page/candidate-page.module';
+import {ManagerPageModule} from './pages/manager-page/manager-page.module';
+import {StoreModule} from '@ngrx/store';
+import {reducers} from './+state';
+import {EffectsModule} from '@ngrx/effects';
+import {TestsEffects} from './+state/tests/tests.effects';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        CandidatePageModule,
-        ManagerPageModule,
-        LoginPageModule,
-        SharedModule,
-        StoreModule.forRoot(reducers),
-        NbLayoutModule,
-        NbThemeModule.forRoot(),
-        NbMenuModule.forRoot(),
-        NbSidebarModule.forRoot(),
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CandidatePageModule,
+    ManagerPageModule,
+    LoginPageModule,
+    SharedModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([TestsEffects]),
+    NbLayoutModule,
+    NbThemeModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbSidebarModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/components/user-sidebar/user-sidebar.component.ts
+++ src/app/components/user-sidebar/user-sidebar.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { NbMenuItem, NbMenuService } from "@nebular/theme";
-import { UserProfileInterface } from "../../interfaces/user-profile-interface";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NbMenuItem, NbMenuService } from '@nebular/theme';
+import { UserProfileInterface } from '../../interfaces/user-profile-interface';
 
 @Component({
-    selector: "app-user-sidebar",
-    templateUrl: "./user-sidebar.component.html",
-    styleUrls: ["./user-sidebar.component.css"],
+    selector: 'app-user-sidebar',
+    templateUrl: './user-sidebar.component.html',
+    styleUrls: ['./user-sidebar.component.css'],
 })
 export class UserSidebarComponent implements OnInit {
     public userInfo: UserProfileInterface;
@@ -15,12 +15,12 @@ export class UserSidebarComponent implements OnInit {
 
     constructor(menu: NbMenuService) {
         this.userInfo = {
-            name: "John",
-            surname: "Doe",
-            role: "candidate",
+            name: 'John',
+            surname: 'Doe',
+            role: 'candidate',
             photo:
-                "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjcsPuSpazhAhWPuIsKHdPzDS8QjRx6BAgBEAU&url=https%3A%2F%2Funsplash.com%2Fsearch%2Fphotos%2Fprofile&psig=AOvVaw1ncLWfBOD0JHiOtYkwCvRs&ust=1554118616819258",
-            status: "Application received",
+                'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjcsPuSpazhAhWPuIsKHdPzDS8QjRx6BAgBEAU&url=https%3A%2F%2Funsplash.com%2Fsearch%2Fphotos%2Fprofile&psig=AOvVaw1ncLWfBOD0JHiOtYkwCvRs&ust=1554118616819258',
+            status: 'Application received',
             progress: 40,
         };
 
